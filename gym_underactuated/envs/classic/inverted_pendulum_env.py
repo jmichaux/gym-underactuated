abe83c45b98e6fd9ca747d311151acbb4cb00d9b
Jonathan Michaux <jmichaux@ttic.edu>
2018-12-03 12:02:51 -0600

    add more general linearization and dynamics

--- gym_underactuated/envs/classic/inverted_pendulum_env.py
+++ gym_underactuated/envs/classic/inverted_pendulum_env.py
@@ -37,6 +37,9 @@ class InvertedPendulumEnv(gym.Env):
         self.b = 0.0
         self.n_coords = 2
 
+        # precompute the jacobian of the dynamics
+        self.jacobian = self._jacobian()
+
         # Angle at which to fail the episode
         if self.task == "balance":
             self.theta_threshold_radians = 12 * 2 * math.pi / 360
@@ -62,6 +65,13 @@ class InvertedPendulumEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
+    def reset(self):
+        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        self.steps_beyond_done = None
+        if self.task == "balance":
+            self.state[1] += np.pi
+        return np.array(self.state)
+
     def is_done(self):
         x, theta = self.state[:2]
         if self.task == "balance":
@@ -73,7 +83,6 @@ class InvertedPendulumEnv(gym.Env):
             pass
         return bool(done)
 
-
     def step(self, action):
         #TODO: assert action is a scalar
         # assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
@@ -84,17 +93,18 @@ class InvertedPendulumEnv(gym.Env):
 
         # clip torque, update dynamics
         u = np.clip(action, -self.force_mag, self.force_mag)
-        xacc, thetaacc = self._dyn2(np.array([x, theta, x_dot, theta_dot]), u)
+        acc = self._accels(anp.array([x, theta, x_dot, theta_dot, u]))
 
         # integrate
+        xacc, thetaacc = acc[0], acc[1]
         x  = x + self.tau * x_dot
         x_dot = x_dot + self.tau * xacc
         theta = theta + self.tau * theta_dot + 0.5 * self.tau**2 * thetaacc
-        self._unwrap_angle(theta)
         theta_dot = theta_dot + self.tau * thetaacc
 
         # update state
-        self.state = np.array([x, theta, x_dot,theta_dot])
+        self._unwrap_angle(theta)
+        self.state = np.array([x, theta, x_dot, theta_dot])
 
         done = self.is_done()
 
@@ -112,12 +122,113 @@ class InvertedPendulumEnv(gym.Env):
 
         return self.state, reward, done, {}
 
-    def reset(self):
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.steps_beyond_done = None
-        if self.task == "balance":
-            self.state[1] += np.pi
-        return np.array(self.state)
+    def _accels(self, vec):
+        """
+        Calculate the accelerations
+        """
+
+        force = vec[-1]
+        pos = vec[:self.n_coords]
+        vel = vec[self.n_coords:-1]
+        state = vec[:-1]
+        Minv = self._Minv(pos)
+        B = self._B()
+        C = self._C(state)
+        G = self._G(pos)
+        acc = anp.dot(Minv, anp.dot(B, force) - anp.dot(C, vel.reshape((2,1))) - G)
+        return acc.flatten()
+
+    def _F(self, vec):
+        """
+        Return derivative of state-space vector
+        """
+        qd = vec[self.n_coords:-1]
+        qdd = self._accels(vec)
+
+        return anp.array(list(qd) + list(qdd))
+
+    def _M(self, pos):
+        """
+        Inertial Mass matrix
+        """
+        x, th = pos
+        I = self.inertial
+        d0 = self.total_mass
+        d1 = self.polemass_length * anp.cos(th)
+
+        mass_matrix = anp.array([[d0, d1],
+                               [d1, I]])
+        return mass_matrix
+
+    def _C(self, state):
+        """
+        Coriolis matrix
+        """
+        x, theta, xdot, thetadot = state
+        d1 = self.polemass_length * thetadot * anp.sin(theta)
+        return anp.array([
+                        [0, -d1],
+                        [0, 0]
+                        ])
+
+    def _G(self, pos):
+        """
+        Gravitional matrix
+        """
+        x, theta = pos
+        d3 = self.polemass_length * anp.sin(theta) * self.g
+        return anp.array([[0], 
+                        [d3]])
+
+    def _B(self):
+        """
+        Force matrix
+        """
+        return anp.array([[1], [0]])
+
+    def _jacobian(self):
+        """
+        Return the Jacobian of the full state equation
+        """
+        return jacobian(self._F)
+
+    def _linearize(self, vec):
+        """
+        Linearize the dynamics by first order Taylor expansion
+        """
+        F0 = self._F(vec)
+        arr = self.jacobian(vec)
+        A = arr[:, :-1]
+        B = arr[:, -1].reshape((2 * self.n_coords, 1))
+        return F0, A, B
+
+    def _Minv(self, pos):
+        """
+        Invert the mass matrix
+        """
+        return anp.linalg.inv(self._M(pos))
+
+    def total_energy(self, state):
+        pos = state[:self.n_coords]
+        vel = state[self.n_coords:]
+        return self.kinetic_energy(pos, vel) + self.potential_energy(pos)
+
+    def kinetic_energy(self, pos, vel):
+        return
+
+    def potential_energy(self, pos):
+        return
+
+    def _unwrap_angle(self, theta):
+        sign = (theta >=0)*1 - (theta < 0)*1
+        theta = anp.abs(theta) % (2 * anp.pi)
+        return sign*theta
+
+    def integrate(self):
+        """
+        Integrate the equations of motion
+        """
+        raise NotImplementedError()
 
     def render(self, mode='human'):
         screen_width = 600
@@ -169,157 +280,3 @@ class InvertedPendulumEnv(gym.Env):
 
     def close(self):
         if self.viewer: self.viewer.close()
-
-
-    def _dyn(self, force):
-        """
-        Calculate the accelerations
-        """
-        x, theta, x_dot, theta_dot = self.state
-        theta = self._unwrap_angle(theta)
-        f = force
-        b = self.b
-        I = self.inertial
-        d0 = self.total_mass
-        d1 = self.polemass_length * np.cos(theta)
-        d2 = self.polemass_length * np.sin(theta) * theta_dot**2
-        d3 = self.polemass_length * np.sin(theta) * self.g
-
-        xacc = ((f + d2) *  I + d1 * d3) / (d0 * I - d1**2)
-        thetaacc = -(d3 + d1 * xacc) / I
-        return xacc, thetaacc
-
-    def _dyn2(self, state, torque):
-        """
-        Calculate the accelerations
-        """
-        u = torque
-        pos = state[:self.n_coords]
-        vel = state[self.n_coords:]
-
-        Minv = self._Minv(pos)
-        B = self._B()
-        C = self._C(pos, vel)
-        G = self._G(pos)
-        acc = np.dot(Minv, B.dot(u) - C.dot(vel.reshape((2,1))) - G)
-        # return acc.flatten()
-        acc = acc.flatten()
-        return acc[0], acc[1]
-
-    def _M(self, pos):
-        """
-        Inertial Mass matrix
-        """
-        x, th = pos
-
-        th = self._unwrap_angle(th)
-        I = self.inertial
-        d0 = self.total_mass
-        d1 = self.polemass_length * np.cos(th)
-
-        mass_matrix = np.array([[d0, d1],
-                               [d1, I]])
-        return mass_matrix
-
-    def _Minv(self, pos):
-        """
-        Invert the mass matrix
-        """
-        return np.linalg.inv(self._M(pos))
-
-    def _C(self, pos, vel):
-        """
-        Coriolis matrix
-        """
-        x, theta = pos
-        x_dot, theta_dot = vel
-        theta = self._unwrap_angle(theta)
-        d1 = self.polemass_length * theta_dot * np.sin(theta)
-        return np.array([
-                        [0, -d1],
-                        [0, 0]
-                        ])
-
-    def _G(self, pos):
-        """
-        Gravitional matrix
-        """
-        x, theta = pos
-        d3 = self.polemass_length * np.sin(theta) * self.g
-
-        return np.array([[0], 
-                        [d3]])
-
-    def _G2(self, pos):
-        """
-        Gravitional matrix
-        """
-        return np.array([[0], 
-                        [self.polemass_length * np.sin(pos[1]) * self.g]])
-
-    def _B(self):
-        """
-        Force matrix
-        """
-        return np.array([[1], [0]])
-
-    def _linearize(self, state):
-        """
-        Linearize the system dynamics around a given point
-        """
-        pos = state[0:self.n_coords]
-        return self._Alin(state), self._Blin(pos)
-
-    def _Alin(self, state):
-        pos = state[:self.n_coords]
-        vel = state[self.n_coords:]
-        ul = np.zeros((self.n_coords, self.n_coords))
-        ur = np.eye(self.n_coords)
-        Minv = self._Minv(pos)
-        deltaG = self._deltaG()
-        C = self._C(pos, vel)
-        ll = -np.dot(Minv, deltaG)
-        lr = -np.dot(Minv, C)
-        return np.block([[ul, ur],
-                        [ll, lr]])
-
-    def _Blin(self, pos):
-        Z = np.dot(self._Minv(pos), self._B())
-        return np.block([
-                        [np.zeros_like(Z)],
-                        [Z]
-                        ])
-
-    def _deltaG(self):
-        m = self.masspole
-        g = self.g
-        l = self.l
-        return np.array([
-                        [0, 0],
-                        [0, -m*g*l]
-                        ])
-        
-
-    def total_energy(self, state):
-        pos = state[:self.n_coords]
-        vel = state[self.n_coords:]
-        return self.kinetic_energy(pos, vel) + self.potential_energy(pos)
-
-    def kinetic_energy(self, pos, vel):
-        return
-
-    def potential_energy(self, pos):
-        return
-
-    def _unwrap_angle(self, theta):
-        sign = (theta >=0)*1 - (theta < 0)*1
-        theta = np.abs(theta) % (2 * np.pi)
-        return sign*theta
-
-    def integrate(self):
-        """
-        Integrate the equations of motion
-        """
-        raise NotImplementedError()
-
-
